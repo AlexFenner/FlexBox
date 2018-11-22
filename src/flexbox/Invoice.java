32f7ce9d66c0e4080c3857757c95636653bd5a8e
anguse207 <angusedmunds1@gmail.com>
2018-11-22 16:48:56 +0000

    Update Invoice.java

--- src/flexbox/Invoice.java
+++ src/flexbox/Invoice.java
@@ -3,15 +3,25 @@ package flexbox;
 import java.util.ArrayList;
 
 public class Invoice extends Order {
-
     public ArrayList<Box> boxes = new ArrayList<Box>();
+    public double totalPrice;
+
     //Constructor
-    Invoice(ArrayList<Box> orderbox) {
+    Invoice(ArrayList<Box> orderbox, double price) {
         boxes = orderbox;
+        double totalPrice = price;
     }
     public ArrayList getBoxes(){
         return boxes;
     }
-    
 
+    public void addToTotal(Box box){
+        //
+        totalPrice += box.getCost(box);
+    }
+
+    public void takeFromTotal(Box box){
+        //
+        totalPrice -= box.getCost(box);
+    }
 }
