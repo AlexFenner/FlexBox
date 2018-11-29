7acd69e2795434b2349b4b75ef6d1aa07bd90749
alexfenner <alexfenner@10.128.107.61>
2018-11-29 12:30:33 +0000

    Documentation for Box class

--- src/flexbox/Box.java
+++ src/flexbox/Box.java
@@ -1,5 +1,14 @@
+/**
+ *The Abstract Box class is used as a template for the different box types
+ *(box1 - box5). Some methods are overridden  by the subclasses. The Box class
+ * is a child class of the main Order class as an order contains many boxes.
+ *
+ */
 package flexbox;
 
+/**
+ * @author Group A4
+ */
 public abstract class Box extends Order {
 
     int ID;
@@ -13,11 +22,23 @@ public abstract class Box extends Order {
     boolean sealable;
     int quantity;
 
-    //Constructor
+    /**
+     * The constructor for Box class
+     *
+     * @param boxLength The length of the box
+     * @param boxWidth The width of the box
+     * @param boxHeight The height of the box
+     * @param boxGrade The grade of the box
+     * @param boxColour The number of colours the box has
+     * @param btmRein Whether or not the box has a reinforced bottom
+     * @param crnrRein Whether or not the box has reinforced corners
+     * @param boxSeal Whether or not the box has a sealable top
+     * @param qnty The number of boxes
+     */
     Box(double boxLength, double boxWidth, double boxHeight, int boxGrade,
-            int boxColour, boolean btmRein, boolean crnrRein, boolean boxSeal, int qnty) {
-        
-        
+            int boxColour, boolean btmRein, boolean crnrRein, boolean boxSeal,
+            int qnty) {
+
         this.length = boxLength;
         this.width = boxWidth;
         this.height = boxHeight;
@@ -27,135 +48,198 @@ public abstract class Box extends Order {
         this.cornerReinforcement = crnrRein;
         this.sealable = boxSeal;
         this.quantity = qnty;
-        
-
     }
 
-    //Get and setters
+    /**
+     * @return The length of the box
+     */
     public double getLenght() {
         return length;
     }
 
+    /**
+     * @param len the length of the box to be set
+     */
     public void setLength(double len) {
         length = len;
     }
 
+    /**
+     * @return the width of the box
+     */
     public double getWidth() {
         return width;
     }
 
+    /**
+     * @param wid the width of the box to be set
+     */
     public void setWidth(double wid) {
         width = wid;
     }
 
+    /**
+     * @return the height of the box
+     */
     public double getHeight() {
         return height;
     }
 
+    /**
+     * @param hig the height of the box to be set
+     */
     public void setHeight(double hig) {
         height = hig;
     }
 
+    /**
+     * @return the grade of the box
+     */
     public int getGrade() {
         return grade;
     }
 
+    /**
+     * @param gr the grade of the box to be set
+     */
     public void setGrade(int gr) {
         grade = gr;
     }
 
+    /**
+     * @return the colour of the box (is overridden by child classes)
+     */
     public int getColour() {
         return 0;
     }
 
+    /**
+     * @param col the number of colours to be set for the box
+     */
     public void setColour(int col) {
         colour = col;
     }
-
+    /**
+     * @return whether or not the box has bottom reinforcement
+     * (overridden by child class)
+     */
     public boolean getBottomReinforcement() {
         return false;
     }
-
+    /**
+     * @param bottom value for whether or not the box has bottom reinforcement
+     */
     public void setBottomReinforcement(boolean bottom) {
         bottomReinforcement = bottom;
     }
-
+    /**
+      * @return whether or not the box has corner reinforcement 
+      * (overridden by child class)
+     */
     public boolean getCornerReinforcement() {
         return false;
     }
-
+    /**
+     * @param corner value for whether or not the box has corner reinforcement
+     */
     public void setCornerReinforcement(boolean corner) {
         cornerReinforcement = corner;
     }
-
+    /**
+     * @return whether or not the box has a sealable top
+     */
     public boolean getSealable() {
         return sealable;
     }
-
+    /**
+     * @param seal Setting the value for whether or not the box is sealable
+     */
     public void setSealable(boolean seal) {
         sealable = seal;
     }
-
+    /**
+     * @return the quantity of boxes
+     */
     public int getQuantity() {
         return quantity;
     }
-
+    /**
+     * @param quant the number of boxes to be set
+     */
     public void setQuantity(int quant) {
         quantity = quant;
     }
-
+    /**
+     * @return the area of the box
+     */
     public double getArea() {
-        return 2 * (height * width) + 2 * (height * length) + 2 * (width * length);
+        /**
+         * calculates the area of the box by adding the surface area of each
+         * side together 
+         */
+        return 2 * (height * width) + 2 * (height * length) + 2 * 
+                (width * length);
     }
-    
+    /**
+     * 
+     * @param box the box that the price needs to be calculated for
+     * @return The cost of the box
+     */
     public double getCost(Box box) {
         double cost = 0;
         double area = box.getArea();
         double percentIncrease = 0;
-        
+        /**
+         * Calculates the base price of the box considering the grade
+         */
         switch (grade) {
-            case 1:  cost = 
-                     (0.55*area);
-                     break;
-            case 2:  cost = 
-                     (0.65*area);
-                     break;
-            case 3:  cost = 
-                     (0.82*area);
-                     break;
-            case 4:  cost = 
-                     (0.98*area);
-                     break;
-            case 5:  cost = 
-                     (1.50*area);
-                     break;
+            case 1:
+                cost
+                        = (0.55 * area);
+                break;
+            case 2:
+                cost
+                        = (0.65 * area);
+                break;
+            case 3:
+                cost
+                        = (0.82 * area);
+                break;
+            case 4:
+                cost
+                        = (0.98 * area);
+                break;
+            case 5:
+                cost
+                        = (1.50 * area);
+                break;
         }
-                     
-        System.out.print("grade calc" + cost);
+        /**
+         * Calculates the percentage to be added on based on the attributes of 
+         * the box.
+         */
         
         if (bottomReinforcement == true) {
-            percentIncrease += 0.13;
+            percentIncrease += 0.13; //+13% if the box has bottom reinforcement
         }
         if (cornerReinforcement == true) {
-            percentIncrease += 0.12;
+            percentIncrease += 0.12; //+12% if the box has corner reinforcement
         }
         if (sealable == true) {
-            percentIncrease += 0.10;
+            percentIncrease += 0.10; //+10% if the box has a sealable top
         }
         if (colour == 1) {
-            percentIncrease += 0.12;
-        } 
-        if (colour == 2) {
-            percentIncrease += 0.15;
+            percentIncrease += 0.12; //+12% if the box has 1 colour
         }
+        if (colour == 2) {
+            percentIncrease += 0.15;//+15% if the box has 2 colours
+        }
+
+        cost = cost * (percentIncrease + 1);// adds on price from above values
+
         
-        
-        
-        cost = cost * (percentIncrease + 1);
-        
-        System.out.print("percent increase" + cost);
-        cost *= box.getQuantity();
-        
+        cost *= box.getQuantity(); // Multiplies the price by number of boxes
+
         return cost;
 
     }
