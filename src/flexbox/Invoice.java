a2b446f264201b0ca26969ad90e43078220d9c5d
alexfenner <alexfenner@10.128.107.61>
2018-11-29 14:07:13 +0000

    Further fixes to final receipt

--- src/flexbox/Invoice.java
+++ src/flexbox/Invoice.java
@@ -67,42 +67,48 @@ public class Invoice extends Order {
      */
     public static void printReciept(ArrayList<Box> boxList) {
         int numOfBoxes = boxList.size();
-        System.out.println("FlexBox Invoice");
-        System.out.println("---------------");
-        System.out.println();
-        System.out.println("Boxes");
-        System.out.println("-----");
-
-        for (int i = 0; i < numOfBoxes; i++) { // prints details for each box
-            Box currBox = boxList.get(i);
-            int id = i+1;
-            System.out.println("ID: " + id);
-            System.out.println("Dimensions: " + currBox.getLenght() + " x "
-                    + currBox.getWidth() + " x " + currBox.getHeight());
-            System.out.println("Grade: " + currBox.getGrade());
-            System.out.println("Number of colours: " + currBox.getColour());
-            //Following if statements priovide more user friendly outputs
-            if (currBox.getBottomReinforcement() == true) {
-                System.out.println("Bottom reinforcement : Yes");
-            } else {
-                System.out.println("Bottom reinforcement : No");
-            }
-            if (currBox.getCornerReinforcement() == true) {
-                System.out.println("Corner reinforcement : Yes");
-            } else {
-                System.out.println("Corner reinforcement : No");
-            }
-            if (currBox.getSealable() == true) {
-                System.out.println("Sealable top : Yes");
-            } else {
-                System.out.println("Sealable top : No");
-            }
-
-            System.out.println("Quantity: " + currBox.getQuantity());
+        if (numOfBoxes > 0) {
+            System.out.println("FlexBox Invoice");
+            System.out.println("---------------");
             System.out.println();
-        }
-        System.out.println("Total Price: £" + String.format("%.2f", totalPrice));
-        System.exit(0); // Closes the program
+            System.out.println("Boxes");
+            System.out.println("-----");
 
+            for (int i = 0; i < numOfBoxes; i++) { // prints details for each box
+                Box currBox = boxList.get(i);
+                int id = i + 1;
+                System.out.println("ID: " + id);
+                System.out.println("Dimensions: " + currBox.getLenght() + " x "
+                        + currBox.getWidth() + " x " + currBox.getHeight());
+                System.out.println("Grade: " + currBox.getGrade());
+                System.out.println("Number of colours: " + currBox.getColour());
+                //Following if statements priovide more user friendly outputs
+                if (currBox.getBottomReinforcement() == true) {
+                    System.out.println("Bottom reinforcement : Yes");
+                } else {
+                    System.out.println("Bottom reinforcement : No");
+                }
+                if (currBox.getCornerReinforcement() == true) {
+                    System.out.println("Corner reinforcement : Yes");
+                } else {
+                    System.out.println("Corner reinforcement : No");
+                }
+                if (currBox.getSealable() == true) {
+                    System.out.println("Sealable top : Yes");
+                } else {
+                    System.out.println("Sealable top : No");
+                }
+
+                System.out.println("Quantity: " + currBox.getQuantity());
+                System.out.println();
+            }
+            System.out.println("Total Price: £" + String.format("%.2f", totalPrice));
+            System.exit(0); // Closes the program
+        }
+        else{
+            System.out.println("No boxes ordered");
+            System.exit(0); // Closes the program
+            
+        }
     }
 }
